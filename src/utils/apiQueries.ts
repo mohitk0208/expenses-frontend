062beade2e755f21ab63ab028c322918481b0ae2
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-01-17 11:41:07 +0530

    converted queries to send variables object

--- src/utils/apiQueries.ts
+++ src/utils/apiQueries.ts
@@ -1,12 +1,19 @@
-export const GET_CATEGORIES = `
-{
-  categories {
-    id,
-    name,
-    backgroundUrl
+export const GET_CATEGORIES = () => {
+  return {
+    query: `
+    {
+      categories {
+        id,
+        name,
+        backgroundUrl
+      }
+    }
+    `,
+    variables: {}
   }
 }
-`
+
+
 
 export const GET_EXPENSES_BY_CATEGORY = (categoryId: string) => `
 {
@@ -20,3 +27,25 @@ export const GET_EXPENSES_BY_CATEGORY = (categoryId: string) => `
   }
 }
 `
+
+export const ADD_CATEGORY = (
+  categoryName: string,
+  backgroundUrl?: string,
+  description: string = ""
+) =>  ({
+  query:`
+  mutation addNewCategory($name: String!, $backgroundUrl: String, $description: String) {
+    addCategory(name: $name, backgroundUrl: $backgroundUrl, description: $description) {
+      id
+      name
+      backgroundUrl
+      description
+    }
+  }
+  `,
+  variables: {
+    name: categoryName,
+    backgroundUrl: backgroundUrl,
+    description: description
+  }
+})
